778e1938e1fed70ace0d1bb2ca65ee7a49c98d01
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-06-05 21:16:08 +0600

    add some file

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Test
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(123456);
+        // Console.WriteLine(123456);
 
 
         // /*
@@ -18,12 +18,36 @@ class Test
         // \\ : to print \
 
         // */
-        // // variable amd data type 8th video
+        // // type casting
 
-        string input = "10";
-        bool isSucess = int.TryParse(input, out int result);
-        Console.WriteLine($"is it Sucessfull:{isSucess}");
-        Console.WriteLine($"number2 = {result}");
-        Console.ReadKey();
+        // string input = "10";
+        // bool isSucess = int.TryParse(input, out int result);
+        // Console.WriteLine($"is it Sucessfull:{isSucess}");
+        // Console.WriteLine($"number2 = {result}");
+        // Console.ReadKey();
+
+        // string? studentName = Console.ReadLine();
+        // Console.WriteLine("student Name: " + studentName);
+
+
+
+        string? studentName;
+        int studentAge;
+        Double gpa;
+
+
+        Console.Write($"enter your Name: ");
+        studentName = Console.ReadLine();
+
+        Console.Write($"Enter your age: ");
+        studentAge = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("enter Your Gpa");
+        gpa = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Name: " + studentName);
+        Console.WriteLine("Age: " + studentAge);
+        Console.WriteLine("Students GPA: " + gpa);
+        
      }
 }
